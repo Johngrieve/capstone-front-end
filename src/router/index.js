3e2b3e17f1f0c1edc6352f4aadc57e09437833a4
John Grieve <jonathanrgrieve@gmail.com>
2022-05-13 14:14:05 -0500

    adds router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-
+import SignUp from "../views/SignUp.vue";
+import LogIn from "../views/LogIn.vue";
+import LogOut from "../views/LogOut.vue";
+import AccountsIndex from "../views/AccountsIndex.vue";
+import AccountsNew from "../views/AccountsNew.vue";
+import AccountsShow from "../views/AccountsShow.vue";
+import AccountsEdit from "../views/AccountsEdit.vue";
 const routes = [
   {
     path: "/",
@@ -14,6 +20,41 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: SignUp,
+  },
+  {
+    path: "/Login",
+    name: "Login",
+    component: LogIn,
+  },
+  {
+    path: "/logout",
+    name: "logout",
+    component: LogOut,
+  },
+  {
+    path: "/accounts",
+    name: "accounts-index",
+    component: AccountsIndex,
+  },
+  {
+    path: "/accounts/new",
+    name: "accounts-new",
+    component: AccountsNew,
+  },
+  {
+    path: "/accounts/:id",
+    name: "accounts-show",
+    component: AccountsShow,
+  },
+  {
+    path: "/accounts/:id/edit",
+    name: "accounts-edit",
+    component: AccountsEdit,
   },
 ];
 
